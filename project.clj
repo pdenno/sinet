bf5c7153a760b2e14f55236d333620f7f2266c3e
Peter Denno <podenno@gmail.com>
2017-05-01 18:30:58 -0400

    individuals WIP

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/core.memoize "0.5.9"]
+                 ;[org.clojure/core.memoize "0.5.9"]
                  [medley "0.8.4"]
                  [zip-visit "1.1.0"]
                  [gov.nist/spntools "0.1.0-SNAPSHOT"]])
