e83e49e9da614cfcb6a4c160dda78e6198d95551
Peter Denno <podenno@gmail.com>
2017-06-30 15:53:26 -0400

    clean up clj and cljs directories

--- project.clj
+++ project.clj
@@ -4,45 +4,90 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :global-vars {*assert* true}
-  :dependencies [[org.clojure/clojure       "1.8.0"]
-                 [org.clojure/clojurescript "1.9.562"]
-                 [org.clojure/core.async    "0.3.443"]
-                 [com.cemerick/piggieback   "0.2.2"] ; sente exlusions https://github.com/ptaoussanis/sente/issues/277
-                 [com.taoensso/sente        "1.11.0" :exclusions [com.taoensso/encore]]
-                 [com.taoensso/timbre       "4.10.0" :exclusions [com.taoensso/encore]]
-                 [com.taoensso/encore       "2.91.1"] ; 2.91.1 is modern but 2.87.0 for issue 277 -- NG
-                 [http-kit                  "2.2.0"]
-                 [ring                      "1.6.1"]
-                 [ring/ring-defaults        "0.3.0"]
-                 [hiccup                    "1.0.5"]
-                 [compojure                 "1.6.0"]
-                 [org.clojure/tools.reader  "1.0.0"]
-                 [medley                    "1.0.0"]
-                 [gov.nist/spntools         "0.1.0-SNAPSHOT"]
-                 [quil                      "2.6.0"]]
+  :dependencies [[org.clojure/clojure        "1.8.0"]
+                 [org.clojure/tools.reader   "1.0.0"] ; this one can probably go. 
+                 [org.clojure/clojurescript  "1.9.660"]
+                 [medley                     "1.0.0"]
+                 [org.clojure/core.async     "0.3.443"]
+                 [com.cemerick/piggieback    "0.2.2"] ; sente exlusions https://github.com/ptaoussanis/sente/issues/277
+                 [com.stuartsierra/component "0.3.2"]
+                 [environ "1.1.0"]
+                 [ch.qos.logback/logback-classic "1.2.3"] ; this one might go.
+                 [org.clojure/tools.logging      "0.4.0"]      ; likewise this one. 
+                 [com.taoensso/sente             "1.11.0" :exclusions [com.taoensso/encore]]
+                 [com.cognitect/transit-clj      "0.8.300"]  ; from rente
+                 [com.cognitect/transit-cljs     "0.8.239"] ; from rente
+
+                 [com.taoensso/timbre        "4.10.0" :exclusions [com.taoensso/encore]]
+                 [com.taoensso/encore        "2.91.1"] ; 2.91.1 is modern but 2.87.0 for issue 277 -- NG
+                 
+                 [ring                       "1.6.1" :exclusions [org.clojure/tools.namespace]]
+                 [ring/ring-defaults         "0.3.0"]
+                 [hiccup                     "1.0.5"] ; This goes away when reagent has proven itself. 
+                 [compojure                  "1.6.0"]
+                 [http-kit                   "2.2.0"]
+
+                 [gov.nist/spntools          "0.1.0-SNAPSHOT" :exclusions [org.clojure/clojure]]
+                 [quil                       "2.6.0"]
+                 [reagent                    "0.7.0"]]
+
   :plugins [[lein-figwheel "0.5.10"]
             [lein-cljsbuild "1.1.4"]]
-  :profiles {:dev {:dependencies [[com.cemerick/piggieback "0.2.2"]
-                                  [figwheel-sidecar "0.5.4-6"]] ; POD added :profiles for figwheel-sidecar
-                   :source-paths ["src/cljs" "cljs_src"]}}
+  :hooks [leiningen.cljsbuild]
+  
+  :source-paths ["src"]
+  :resource-paths ["resources" "resources-index/prod"]
+  :target-path "target/%s"
+
+  :main ^:skip-aot gov.nist.sinet.run
+
+  :cljsbuild
+  {:builds
+   {:client {:source-paths ["src/gov/nist/sinet/client"]
+             :figwheel true
+             :compiler
+             {:output-to "resources/public/js/app.js"
+              :output-dir "dev-resources/public/js/out"}}}}
+  
+  :profiles {:dev-config {}
+
+             :dev [:dev-config
+                   {:dependencies [[org.clojure/tools.namespace "0.2.10"]
+                                   [com.cemerick/piggieback "0.2.2"] ; doesn't belong inside client nor figwheel
+                                   [figwheel "0.5.10"]
+                                   [figwheel-sidecar "0.5.10"]]
+                    :plugins [[lein-figwheel "0.5.10"]
+                              [lein-environ "1.0.1"]]
+                              
+                    :source-paths ["dev"]
+                    :resource-paths ^:replace
+                    ["resources" "dev-resources" "resources-index/dev"]
+
+                    :cljsbuild
+                    {:builds
+                     {:client {:source-paths ["dev"]
+                               :compiler
+                               {:optimizations :none
+                                :source-map true}}}}
+
+                    :figwheel {:http-server-root "public"
+                               :server-port 3449
+                               :repl false
+                               :css-dirs ["resources/public/css"]}}]
+
+             :prod {:cljsbuild
+                    {:builds 
+                     {:client {:compiler {:optimizations :advanced
+                                          :elide-asserts true
+                                          :pretty-print false}}}}}}
   :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
-  :source-paths ["src/clj" "cljs_src"]
-  :cljsbuild {:builds
-              [{:id :cljs-client
-                :figwheel true
-                :source-paths ["src/cljs"]
-                :compiler {:main "gov.nist.sinet.util.client"
-                           :asset-path "js/out"
-                           :output-to  "resources/public/js/main.js"
-                           :output-dir "resources/public/js/out" 
-                           :optimizations :none #_:whitespace #_:advanced ; POD commented out :whitespace, added :none for figwheel
-                           :pretty-print true}}]}
-  :figwheel {:nrepl-port 8877 :css-dirs ["css"]} 
-  :main gov.nist.sinet.util.server
+  ;:figwheel {:nrepl-port 8877 :css-dirs ["css"]} 
+  ;:main gov.nist.sinet.util.server ; POD was util.server ???
   ;; Call `lein start-repl` to get a (headless) development repl that you can
   ;; connect to with Cider+emacs or your IDE of choice:
   :aliases  {"start-repl" ["do" "clean," "cljsbuild" "once," "repl" ":headless"]
-             "start"      ["do" "clean," "cljsbuild" "once," "run"]})
+             "start"      ["do" "clean," "cljsbuild" "once," "run"]
+             "package"    ["with-profile" "prod" "do" "clean" ["cljsbuild" "once"]]})
 
 
 
